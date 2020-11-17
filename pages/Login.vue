832dc0b8fab43b748349f167489ded512a6070c1
HirosueAkari <hirosue_akari@retail-ai.jp>
2020-11-17 13:29:31 +0900

    asyncDataを使用

--- pages/Login.vue
+++ pages/Login.vue
@@ -1,6 +1,7 @@
 <template>
   <v-container>
     <LoginCard @emitLogin="login" />
+    {{ data }}
   </v-container>
 </template>
 <script lang="ts">
@@ -9,6 +10,13 @@ import { Component, Vue } from 'nuxt-property-decorator'
 export default class Login extends Vue {
   login() {
     this.$router.push('/home')
+  }
+
+  async asyncData({ app }: any) {
+    const { data } = await app.$axios.get(
+      'https://jsonplaceholder.typicode.com/users'
+    )
+    return { data: data[0] }
   }
 }
 </script>
